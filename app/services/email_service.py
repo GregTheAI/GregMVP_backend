94cfe89f41aae828c3b6acca374a3a680687b876
rachealai <rachael@gregthe.ai>
2025-07-03 17:38:27 +0000

    📧added implementation to send email for waitlist

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,4 +1,9 @@
+from email.message import EmailMessage
+
 import boto3
+import certifi
+import ssl
+from aiosmtplib import SMTP
 from botocore.client import BaseClient
 from botocore.exceptions import ClientError
 from fastapi import BackgroundTasks
@@ -9,31 +14,32 @@ settings = get_settings()
 
 
 class EmailService:
-    def __init__(self):
-        self.ses_client: BaseClient = boto3.client(
-            "ses",
-            region_name=settings.AWS_REGION,
-            aws_access_key_id=settings.AWS_ACCESS_KEY,
-            aws_secret_access_key=settings.AWS_SECRET_KEY
-        )
 
-    def _send_email_sync(self, to_email: str, subject: str, html_body: str):
+    @staticmethod
+    async def _send_email_sync(to_email: str, subject: str, html_body: str):
+        ssl_context = ssl.create_default_context(cafile=certifi.where())
+        smtp = SMTP(hostname=settings.SMTP_HOST, port=settings.SMTP_PORT, start_tls=settings.SMTP_TLS, tls_context=ssl_context)
+
         try:
-            response = self.ses_client.send_email(
-                Source=settings.EMAILS_FROM_EMAIL,
-                Destination={"ToAddresses": [to_email]},
-                Message={
-                    "Subject": {"Data": subject, "Charset": "UTF-8"},
-                    "Body": {
-                        "Html": {"Data": html_body, "Charset": "UTF-8"},
-                        "Text": {"Data": "Please use an HTML-compatible viewer", "Charset": "UTF-8"},
-                    },
-                },
-            )
-            return response
+            message = EmailMessage()
+            message["From"] = settings.EMAILS_FROM_EMAIL
+            message["To"] = to_email
+            message["Subject"] = subject
+            message.set_content("Sent from GregAI")
+            message.add_alternative(html_body, subtype="html")
+
+            await smtp.connect()
+            # await smtp.login(settings.SMTP_USER, settings.SMTP_PASSWORD)
+            await smtp.login("ses-smtp-user.20250703-111313", "BFkvgWwvPT83A2nHTjm1Lunf9sSgv9CM2TdSieXLmukk")
+            smtp_response = await smtp.send_message(message)
+            return smtp_response
+
         except ClientError as e:
             print(f"Failed to send email: {e.response['Error']['Message']}")
-            return None
+            return await smtp.quit()
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+            return await smtp.quit()
 
     def send_email_background(self, background_tasks: BackgroundTasks, to_email: str, subject: str, html_content: str):
         background_tasks.add_task(self._send_email_sync, to_email, subject, html_content)
